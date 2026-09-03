8481535d85adc11a8606e44aa50a7320c8482ba6
ritsu1201 <ritsu1201k@outlook.jp>
2026-09-04 08:04:46 +0900

    feat: スクロールアニメーションの準備

---
+++ story/story.js
@@ -0,0 +1,5 @@
+console.log("connect");
+// スクロールで表示する処理をまとめて取得
+const reveals = document.querySelectorAll(".reveal");
+// 画面内に入った要素を検知して表示アニメーションを実行
+const observer = new IntersectionObserver((entries) => {});
