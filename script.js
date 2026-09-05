2946251572bbfde1734b96b6731d99b74c507fcd
ritsu1201 <ritsu1201k@outlook.jp>
2026-09-05 09:11:00 +0900

    feat: スクロール表示アニメーションを実装

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ console.log("connect");
 const track = document.querySelector(".hero-track");
 const dots = document.querySelectorAll(".slide-dots span");
 console.log(dots);
-let startX = 0;
 // 指を置いた瞬間のⅹ座標
-let endX = 0;
+let startX = 0;
 // 指を離した瞬間のⅹ座標
-let currentIndex = 0;
+let endX = 0;
 // 現在何枚目か
+let currentIndex = 0;
 dots[currentIndex].textContent = "●";
 function showSlide() {
   track.style.transform = `translateX(-${currentIndex * 100}%)`;
@@ -19,10 +19,11 @@ function rendedrDots() {
   dots[currentIndex].textContent = "●";
 }
 // currentIndexと画像を接続
+// //.hero-track に指が触れた瞬間、指のX座標を startX に保存する
 track.addEventListener("touchstart", (e) => {
   startX = e.touches[0].clientX;
 });
-//.hero-track に指が触れた瞬間、指のX座標を startX に保存する
+// 指を離した瞬間のX座標を endX に保存する
 track.addEventListener("touchend", (e) => {
   endX = e.changedTouches[0].clientX;
   if (startX - endX > 50) {
@@ -50,4 +51,28 @@ track.addEventListener("touchend", (e) => {
     }
   }
 });
-// 指を離した瞬間のX座標を endX に保存する
+const reveals = document.querySelectorAll(".reveal");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      if (entry.target.classList.contains("title")) {
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 500);
+      }
+      if (entry.target.classList.contains("date")) {
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 800);
+      }
+      if (entry.target.classList.contains("btn")) {
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 1300);
+      }
+    }
+  });
+});
+reveals.forEach((reveal) => {
+  observer.observe(reveal);
+});
--- story/story.js
+++ story/story.js
@@ -1,5 +1,58 @@
 console.log("connect");
 // スクロールで表示する処理をまとめて取得
 const reveals = document.querySelectorAll(".reveal");
-// 画面内に入った要素を検知して表示アニメーションを実行
-const observer = new IntersectionObserver((entries) => {});
+// 画面内に入った要素を検知して表示アニメーションを実行    
+// 監視員を作り、仕事内容を設定
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    // 監視対象が画面と交錯していたら実行
+    // 画面外にいても監視はしている。
+    if (entry.isIntersecting) {
+      if (entry.target.classList.contains("date")) {
+        // それぞれ時間差で表示されるようにする
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 500);
+      } else if (entry.target.classList.contains("title")) {
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 1500);
+      } else if (entry.target.classList.contains("card-1")) {
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 800);
+      } else if (entry.target.classList.contains("photo-date")) {
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 1000);
+      } else if (entry.target.classList.contains("location")) {
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 1400);
+      } else if (entry.target.classList.contains("memory-text")) {
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 1800);
+      }
+      else if (entry.target.classList.contains("profile")) {
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 500);
+      }
+      else if (entry.target.classList.contains("ritsu-card")) {
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 800);
+      }
+      else if (entry.target.classList.contains("sara-card")) {
+        setTimeout(() => {
+          entry.target.classList.add("active");
+        }, 800);
+      }
+    }
+  });
+});
+reveals.forEach((reveal) => {
+  //   監視員に何を監視するのか指示する
+  observer.observe(reveal);
+});
